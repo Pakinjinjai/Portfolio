1b6af270dbc88f1fb3bcde056e7bfa03925b8c5d
PAKIN JINJAI <126375863+Pakinjinjai@users.noreply.github.com>
2024-01-31 00:47:26 +0700

    Default

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  
+  return (
+    <div 
+    className={`w-full h-full min-h-[100vh] 
+  bg-white ${darkMode && "dark"}`}>
+    <div
+    className='dark:bg-gradient-to-tr from-[#0235a3] via-[#030a1c] to-[#05174e]'
+    >
+      Home 
+    </div>
+    </div>
+  );
+}
+
+export default App;
